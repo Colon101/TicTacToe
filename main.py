f19262c020fca8686baec495c9f758c0e19d9829
Colon101 <kfirgoldman12@gmail.com>
2023-07-06 16:17:20 +0300

    final Tic Tac Toe singleplayer game

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from tkinter import *
+from tkinter import messagebox
+import os
 turn = "X"
 def check_winner():
     for i in range(3):
-        if buttons[i][0]['text'] == buttons[i][1]['text'] == buttons[i][2]['text'] == "❌":
+        if buttons[i][0]['text'] == buttons[i][1]['text'] == buttons[i][2]['text'] and \
+           buttons[i][0]['text'] in ["❌", "⭕"]:
             return True
     for i in range(3):
-        if buttons[0][i]['text'] == buttons[1][i]['text'] == buttons[2][i]['text'] == "❌":
+        if buttons[0][i]['text'] == buttons[1][i]['text'] == buttons[2][i]['text'] and \
+           buttons[0][i]['text'] in ["❌", "⭕"]:
             return True
-    if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons[2][2]['text'] == "❌" or \
-            buttons[0][2]['text'] == buttons[1][1]['text'] == buttons[2][0]['text'] == "❌":
+    if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons[2][2]['text'] and \
+       buttons[0][0]['text'] in ["❌", "⭕"] or \
+       buttons[0][2]['text'] == buttons[1][1]['text'] == buttons[2][0]['text'] and \
+       buttons[0][2]['text'] in ["❌", "⭕"]:
         return True
     return False
+
 
 def button_click(row, col):
     global turn
@@ -22,7 +29,11 @@ def button_click(row, col):
         buttons[row][col].config(text="⭕")
     buttons[row][col].config(state="disabled")
     if check_winner():
-        print("Player has made a cross!")
+        if turn == "X":
+            messagebox.showinfo("⭕","Player ⭕ Has Won!")
+        else:
+            messagebox.showinfo("❌","Player ❌ Has Won!")
+            os._exit(1)
 
 window = Tk()
 window.title("Tic-Tac-Toe")
