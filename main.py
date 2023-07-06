51efb072598d3f51b39e0ae1465f04dd59860282
Colon101 <kfirgoldman12@gmail.com>
2023-07-06 16:10:19 +0300

    removed useless comments

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from tkinter import *
 
 def check_winner():
-    # Check rows
     for i in range(3):
         if buttons[i][0]['text'] == buttons[i][1]['text'] == buttons[i][2]['text'] == "❌":
             return True
-    # Check columns
     for i in range(3):
         if buttons[0][i]['text'] == buttons[1][i]['text'] == buttons[2][i]['text'] == "❌":
             return True
-    # Check diagonals
     if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons[2][2]['text'] == "❌" or \
             buttons[0][2]['text'] == buttons[1][1]['text'] == buttons[2][0]['text'] == "❌":
         return True
@@ -20,7 +17,6 @@ def button_click(row, col):
     buttons[row][col].config(state="disabled")
     if check_winner():
         print("Player has made a cross!")
-        # You can perform any additional actions here when a cross is made
 
 window = Tk()
 window.title("Tic-Tac-Toe")
